32732df7e5a29879b8b7a91e46cdd95c94044cf1
Alex <alex@dirtyunicorns.com>
2018-04-29 21:45:08 -0500

    Add vendor patch level system property
    Change-Id: Ib4470a7beae724de3d9f46127d9880a542b72dd4

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -53,6 +53,14 @@ public class Build {
     /** The name of the underlying board, like "goldfish". */
     public static final String BOARD = getString("ro.product.board");
 
+    /** The build date
+     * @hide
+     */
+    public static final String DATE = getString("ro.build.date");
+
+    /** The vendor patch level. */
+    public static final String VENDOR_PATCH_LEVEL = getString("ro.vendor.patch.level");
+
     /**
      * The name of the instruction set (CPU type + ABI convention) of native code.
      *
